4cce2fa4c814beb4727a17534ed47e6f154558a5
TheDarkDnKTv <x22dnk@hotmail.com>
2023-02-08 22:32:20 +0900

    Init 2

---
+++ src/main/java/nikita/test/unitaz/UnitazMod.java
@@ -0,0 +1,18 @@
+package nikita.test.unitaz;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = UnitazMod.MODID, version = UnitazMod.VERSION)
+public class UnitazMod {
+    public static final String MODID = "unitaz";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+		// some example code
+
+    }
+}
