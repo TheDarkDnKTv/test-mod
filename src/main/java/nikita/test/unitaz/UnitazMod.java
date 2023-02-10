f814889fa66f0f3f70af38b94c2b9ae480432999
TheDarkDnKTv <x22dnk@hotmail.com>
2023-02-10 19:39:21 +0900

    Fixed no textures in IDEA

--- src/main/java/nikita/test/unitaz/UnitazMod.java
+++ src/main/java/nikita/test/unitaz/UnitazMod.java
@@ -8,30 +8,28 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import nikita.test.unitaz.common.CommonProxy;
 
-
-@Mod(modid = UnitazMod.MODID, version = UnitazMod.VERSION)
+@Mod(modid = UnitazMod.MODID, version = UnitazMod.VERSION, name = "Unitaz Mod")
 public class UnitazMod {
+
     public static final String MODID = "unitaz";
-
     public static final String VERSION = "1.0";
-
     @SidedProxy(
-            clientSide = "nikita.test.unitaz.client.ClientProxy",
-            serverSide = "nikita.test.unitaz.common.CommonProxy"
+        clientSide = "nikita.test.unitaz.client.ClientProxy",
+        serverSide = "nikita.test.unitaz.common.CommonProxy"
     )
-
     public static CommonProxy proxy;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-
         proxy.preInit(event);
     }
 
+    @EventHandler
     public void init(FMLInitializationEvent event) {
-		// some example code
         proxy.init(event);
     }
 
+    @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
     }
